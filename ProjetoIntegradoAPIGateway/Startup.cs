fc78d030657c3d922e94ec903454e3e51576d80f
Gleidson Jeferson <gleidsonsantos88@outlook.com>
2022-06-21 19:36:45 -0300

    Add project files.

---
+++ ProjetoIntegradoAPIGateway/Startup.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Text;
+
+namespace ProjetoIntegradoAPIGateway
+{
+    public class Startup
+    {
+        private IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string textKey = "projetoIntegradoPuc";
+
+            var key = Encoding.ASCII.GetBytes(textKey);
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(textKey));
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = "chandra",
+                ValidateAudience = true,
+                ValidAudience = "enduser",
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                RequireExpirationTime = true,
+            };
+
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+              .AddJwtBearer("TestKey", x =>
+              {
+                  x.RequireHttpsMetadata = false;
+                  x.TokenValidationParameters = tokenValidationParameters;
+              });
+
+
+            services.AddOcelot(configuration);
+           
+
+          
+        }
+    
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => {
+
+                    await context.Response.WriteAsync("API Gateway Funcionando");
+                
+                });
+            });
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
